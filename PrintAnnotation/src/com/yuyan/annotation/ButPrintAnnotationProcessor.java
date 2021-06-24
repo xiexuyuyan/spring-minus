07b77b2433fd92ba334fef694aa2bcee69a111d1
xiexuyuyan <2351783158@qq.com>
2021-06-24 20:29:16 +0800

    0.0.4 新建编译时注解架构，在主项目中测试仅打印行为的注解

---
+++ PrintAnnotation/src/com/yuyan/annotation/ButPrintAnnotationProcessor.java
@@ -0,0 +1,26 @@
+package com.yuyan.annotation;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+
+@SupportedAnnotationTypes("com.yuyan.annotation.ButPrint")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class ButPrintAnnotationProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for (TypeElement typeElement : annotations) {
+            for (Element element : roundEnv.getElementsAnnotatedWith(typeElement)) {
+                ButPrint print = element.getAnnotation(ButPrint.class);
+                System.out.println(print.value());
+            }
+        }
+        return true;
+    }
+}
--- application/src/com/yuyan/Application.java
+++ application/src/com/yuyan/Application.java
@@ -1,11 +1,13 @@
 package com.yuyan;
 
+import com.yuyan.annotation.JustPrint;
 import com.yuyan.controllers.SalaryController;
 import com.yuyan.starter.MiniApplication;
 
 public class Application {
+    @JustPrint("Application")
     public static void main(String[] args) {
         System.out.println("hello world");
-        MiniApplication.run(Application.class, args);
+        // MiniApplication.run(Application.class, args);
     }
 }
